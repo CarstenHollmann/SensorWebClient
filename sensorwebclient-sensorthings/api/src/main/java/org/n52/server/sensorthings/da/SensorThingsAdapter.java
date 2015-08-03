82ccc237bd89876868716bd3eaad05668fb0962f
CarstenHollmann <c.hollmann@52north.org>
2015-08-03 14:01:50 +0200

    Init support for SensorThings-API

---
+++ sensorwebclient-sensorthings/api/src/main/java/org/n52/server/sensorthings/da/SensorThingsAdapter.java
@@ -0,0 +1,52 @@
+package org.n52.server.sensorthings.da;
+
+import org.n52.oxf.OXFException;
+import org.n52.oxf.adapter.IServiceAdapter;
+import org.n52.oxf.adapter.OperationResult;
+import org.n52.oxf.adapter.ParameterContainer;
+import org.n52.oxf.ows.ExceptionReport;
+import org.n52.oxf.ows.ServiceDescriptor;
+import org.n52.oxf.ows.capabilities.Operation;
+
+public class SensorThingsAdapter implements IServiceAdapter {
+
+	@Override
+	public ServiceDescriptor initService(String serviceURL)
+			throws ExceptionReport, OXFException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OperationResult doOperation(Operation operation,
+			ParameterContainer parameterContainer) throws ExceptionReport,
+			OXFException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServiceType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getSupportedVersions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getResourceOperationName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
